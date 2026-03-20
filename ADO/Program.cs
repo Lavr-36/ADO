f4e4fd74754de6788d444b99a4d43fbb6ef621ca
КРУГ ЧЕРНЫЙ <lavrentiy36@mail.ru>
2026-03-20 20:37:02 +0300

    Connector done

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -9,36 +9,26 @@ namespace ADO
 {
     internal class Program
     {
+        static SqlConnection connection = null;
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.GetEncoding(1251);
             string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;" +
-                "Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;" +
+                "Integrated Security=True;Connect Timeout=3;Encrypt=False;TrustServerCertificate=False;" +
                 "ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            Console.WriteLine(connection_string);
-            SqlConnection connection = new SqlConnection(connection_string);
-            connection.Open();
+            Connector connector = new Connector(connection_string);
 
             string cmd = "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
-            SqlCommand command = new SqlCommand(cmd, connection);
+            connector.Select(cmd);
+            Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Movies")}");
 
-            SqlDataReader reader = command.ExecuteReader();
-            for (int i = 0; i < reader.FieldCount; i++)
-                Console.Write(reader.GetName(i) + "\t");
-            Console.WriteLine();
-            while (reader.Read())
-            {
-                 //Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
-                for (int i = 0; i < reader.FieldCount; i++)
-                    Console.Write($"{(reader[i])}\t");
-                Console.WriteLine();
-            }
-            reader.Close();
+            connector.Select("SELECT * FROM Directors");
+            Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
+            //command.CommandText = "SELECT COUNT(*) FROM Movies";
+            //command.CommandText = "SELECT COUNT(*) FROM Movies";
+            //Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
 
-            command.CommandText = "SELECT COUNT(*) FROM Movies";
-            Console.WriteLine($"Количество записей:\t{command.ExecuteScalar()}");
-
-            connection.Close();
+            //connection.Close();
         }
     }
 }
