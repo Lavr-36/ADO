9d05dc866fa14de2566026619e9d733c0df1cd61
КРУГ ЧЕРНЫЙ <lavrentiy36@mail.ru>
2026-04-10 20:53:00 +0300

    Edit student DONE

--- Academy/HumanForm.cs
+++ Academy/HumanForm.cs
@@ -1,23 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Academy
 {
-    public abstract partial class HumanForm : Form
-    {
-        protected DBtools.Connector connector;
-        public HumanForm()
-        {
-            InitializeComponent();
-        }
+	public partial class HumanForm : Form
+	{
+		internal Models.Human human;
+		//static protected DBtools.Connector connector;
+		protected HumanForm()
+		{
+			InitializeComponent();
+			//connector = new DBtools.Connector(ConfigurationManager.ConnectionStrings["PV_521_Import"].ConnectionString);
+		}
+		protected void Extract()
+		{
+			if (human != null)
+			{
+                if (human.id != 0) labelID.Text = $"ID:{human.id}";
+                tbLastName.Text = human.last_name;
+				tbFirstName.Text = human.first_name;
+				tbMiddleName.Text = human.middle_name;
+				dtpBirthDate.Value = Convert.ToDateTime(human.birth_date);
+				tbEmail.Text = human.email;
+				tbPhone.Text = human.phone;
+			}
+		}
 
-        protected abstract void buttonOK_Click(object sender, EventArgs e);
-    }
+
+		protected virtual void buttonOK_Click(object sender, EventArgs e)
+		{
+			human = new Models.Human
+				(
+                labelID.Text == "" ? 6 : Convert.ToInt32(labelID.Text.Split(':').Last()),
+                tbLastName.Text,
+				tbFirstName.Text,
+				tbMiddleName.Text,
+				dtpBirthDate.Value.ToString("yyyy-MM-dd"),
+				tbEmail.Text,
+				tbPhone.Text,
+				pbPhoto.Image
+				);
+		}
+	}
 }
--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -17,7 +17,7 @@ namespace Academy
 		{
 			new Query
 				(
-				"last_name,first_name,middle_name,group_name,direction_name",
+				"stud_id,last_name,first_name,middle_name,group_name,direction_name",
 				"Students,Groups,Directions",
 				"[group]=group_id AND direction=direction_id"
 				),
@@ -41,11 +41,12 @@ namespace Academy
 		};
 		DataGridView[] tables;
 		DBtools.Connector connector;
-        /// ///////////////
-        Dictionary<string, int> d_directions;
-        Dictionary<string, int> d_groups;
+		/// ////////////////////////////////////////////////////////////
+		Dictionary<string, int> d_directions;
+		Dictionary<string, int> d_groups;
+
 		StudentForm studentForm;
-        public MainForm()
+		public MainForm()
 		{
 			InitializeComponent();
 			tables = new DataGridView[] { dgvStudents, dgvGroups, dgvDirections, dgvDisciplines, dgvTeachers };
@@ -55,13 +56,14 @@ namespace Academy
 			//toolStripStatusLabel.Text = $"Количество направлений обучения: {dgvDirections.Rows.Count - 1}";
 			//toolStripStatusLabel.Text = $"Количество направлений обучения: {connector.Scalar("SELECT COUNT(*) FROM Directions")}";
 			tabControl_SelectedIndexChanged(tabControl, null);
-            d_directions = connector.GetDictionary("Directions");
-            d_groups = connector.GetDictionary("Groups");
 
-            сbStudentsGroup.Items.AddRange(d_groups.Keys.ToArray());
-            cbGroupsDirection.Items.AddRange(d_directions.Keys.ToArray());
-            cbStudentsDirection.Items.AddRange(d_directions.Keys.ToArray());
-        }
+			d_directions = connector.GetDictionary("Directions");
+			d_groups = connector.GetDictionary("Groups");
+
+			cbStudentsGroup.Items.AddRange(d_groups.Keys.ToArray());
+			cbGroupsDirection.Items.AddRange(d_directions.Keys.ToArray());
+			cbStudentsDirection.Items.AddRange(d_directions.Keys.ToArray());
+		}
 
 		private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
 		{
@@ -70,35 +72,48 @@ namespace Academy
 			toolStripStatusLabel.Text = $"{status_messages[i]}: {tables[i].RowCount - 1}";
 		}
 
-        private void cbGroupsDirection_SelectedIndexChanged(object sender, EventArgs e)
-        {
-			
-            dgvGroups.DataSource = connector.Select
+		private void cbGroupsDirection_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			dgvGroups.DataSource = connector.Select
 				(
-				queries[1].ToString() +
+				queries[1].ToString() + 
 				$" AND direction={d_directions[cbGroupsDirection.SelectedItem.ToString()]}"
 				);
-            toolStripStatusLabel.Text = $"{status_messages[1]}: {dgvGroups.RowCount - 1}";
-        }
+			toolStripStatusLabel.Text = $"{status_messages[1]}: {dgvGroups.RowCount - 1}";
+		}
 
-        private void cbStudentsDirection_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            d_groups = connector.GetDictionary("Groups", $"direction ={d_directions[cbStudentsDirection.SelectedItem.ToString()]}");
-            сbStudentsGroup.Items.Clear();
-            сbStudentsGroup.Items.AddRange(d_groups.Keys.ToArray());
-            dgvStudents.DataSource = connector.Select
+		private void cbStudentsDirection_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			d_groups = connector.GetDictionary
 				(
-				queries[0].ToString() +
-				$" AND direction={d_directions[cbStudentsDirection.SelectedItem.ToString()]}"
+				"Groups",
+				$"direction={d_directions[cbStudentsDirection.SelectedItem.ToString()]}"
 				);
-            toolStripStatusLabel.Text = $"({status_messages[0]}): {dgvStudents.RowCount - 1}";
-        }
+			cbStudentsGroup.Items.Clear();
+			cbStudentsGroup.Items.AddRange(d_groups.Keys.ToArray());
+			dgvStudents.DataSource = connector.Select
+				(
+					queries[0].ToString()+
+					$" AND direction={d_directions[cbStudentsDirection.SelectedItem.ToString()]}"
+				);
+			toolStripStatusLabel.Text = $"{status_messages[0]}: {dgvStudents.RowCount - 1}";
+		}
 
-        private void buttonAddStudent_Click(object sender, EventArgs e)
-        {
+		private void buttonAddStudent_Click(object sender, EventArgs e)
+		{
 			studentForm = new StudentForm();
 			if (studentForm.ShowDialog() == DialogResult.OK)
 				tabControl_SelectedIndexChanged(tabControl, null);
-        }
-    }
+		}
+
+		private void dgvStudents_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+		{
+			StudentForm form = new StudentForm
+				(
+				Convert.ToInt32(dgvStudents.Rows[e.RowIndex].Cells["stud_id"].Value)
+				);
+			if (form.ShowDialog() == DialogResult.OK)
+				tabControl_SelectedIndexChanged(tabControl, null);
+		}
+	}
 }
